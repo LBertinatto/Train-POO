6fc8439fdd66b4416713f0141442dc9cd5e1dc7c
Leonardo Bertinatto <105060693+LBertinatto@users.noreply.github.com>
2022-09-02 18:55:03 -0300

    Update GaragemVagoes.java

--- GaragemVagoes.java
+++ GaragemVagoes.java
@@ -1,11 +1,26 @@
 import java.util.ArrayList;
 
-public class GaragemVagoes {
-    private ArrayList<Vagao> vagoes;
+public class GaragemLocomotivas {
+    private ArrayList<Locomotiva> locomotivas = new ArrayList<>();
 
-    //copiar metodos da garagemlocomotivas, mas para vagoes.
+    /**
+     * construtor padrão que adiciona locomotivas livres na lista.
+     */
+    public GaragemLocomotivas() {
+        locomotivas.add(new Locomotiva(1, 1000, 50));
+        locomotivas.add(new Locomotiva(2, 1000, 50));
+        locomotivas.add(new Locomotiva(3, 1000, 50));
+        locomotivas.add(new Locomotiva(4, 1000, 50));
+        locomotivas.add(new Locomotiva(5, 1000, 50));
+    }
 
+    public Locomotiva buscarLocomotivaLivre(int id) {
+        for (Locomotiva x : locomotivas) {
+            if (id==x.getId() && x.getComposicao()==null) {
+                return x;
+            }
+        }
+        return null;
+    }
 
 }
-
-
